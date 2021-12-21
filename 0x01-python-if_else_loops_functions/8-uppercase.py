b8a092543ceb085f6075170cbaa84c3b7084c3bf
diegojojoayandun <3922@holbertonschool.com>
2021-12-21 17:31:36 -0500

    Add task8 py source file to repo

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            nueva = chr(ord(c) - 32)
+        else:
+            nueva = c
+        print("{:s}".format(nueva), end="")
+    print(' ')
