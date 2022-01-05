be639e4b4e6a9d923e82dee0c0047e5096211dc6
diegojojoayandun <3922@holbertonschool.com>
2022-01-05 12:21:09 -0500

    Add task0 py file

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+          new_matrix = [[j ** 2 for j in i] for i in matrix]
+    return new_matrix
