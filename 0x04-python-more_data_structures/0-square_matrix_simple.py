2f981bf95b499869fc53d0a240f347cf4591ffd5
diegojojoayandun <3922@holbertonschool.com>
2022-01-05 14:19:01 -0500

    Fix Code

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[[]]):
-
     new_matrix = [[j ** 2 for j in i] for i in matrix]
     return new_matrix
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
