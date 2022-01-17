d93d9443fa5c71225d5158c0d1addcd3fd96476a
diegojojoayandun <3922@holbertonschool.com>
2022-01-17 12:02:05 -0500

    Fix code

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-'''Safe printing of integers list'''
-
-
 def safe_print_integer(value):
-    ''' prints an integer with "{:d}".format().'''
     try:
         print('{:d}'.format(value))
         return True
