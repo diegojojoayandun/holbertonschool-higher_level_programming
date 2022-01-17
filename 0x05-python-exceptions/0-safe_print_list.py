6a93c35cbdb760b0856fecac89b0d88c14f90e54
diegojojoayandun <3922@holbertonschool.com>
2022-01-17 12:01:21 -0500

    Fix code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-'''This script prints elements of a list to show the use of exceptions'''
 def safe_print_list(my_list, limit):
-    '''prints x elements of a list return count'''
-
-    count = 0
+    counter = 0
     for i in range(0, limit):
         try:
             print("{:d}".format(my_list[i]), end="")
-            count += 1
+            counter += 1
         except IndexError:
             pass
     print()
-    return count
+    return counter
