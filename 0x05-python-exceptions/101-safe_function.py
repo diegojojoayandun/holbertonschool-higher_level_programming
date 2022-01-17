7a806aa49d901f2f3379bb51e748c62828deb2d0
diegojojoayandun <3922@holbertonschool.com>
2022-01-17 12:11:00 -0500

    Delete docstring and fix two lines after and before import

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+import sys
+
+
+def safe_function(fct, *args):
+    '''executes a function safely.'''
+    try:
+        result = fct(*args)
+        return result
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=sys.stderr)
+        return None
