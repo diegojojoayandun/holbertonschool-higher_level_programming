9c2904d8ec3f76ddd32f70255e1936d9414584c9
diegojojoayandun <3922@holbertonschool.com>
2022-01-17 12:03:57 -0500

    Fix code

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-'''Print and count integers'''
-
-
 def safe_print_list_integers(my_list=[], x=0):
-    '''prints the first x elements of a list and only integers'''
     counter = 0
     for i in range(0, x):
         try:
