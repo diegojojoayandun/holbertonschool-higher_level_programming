c606e8d11349adabcd8c2dceaa198a84f772dc44
diegojojoayandun <3922@holbertonschool.com>
2022-01-18 16:22:08 -0500

    Add task3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """defines a square
+    Private instance attribute: size
+    Instantiation with optional size: def __init__(self, size=0):
+    """
+    def __init__(self, size=0):
+        """Initializes the data with validation"""
+        if not  type(size) == int:
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Square area."""
+        return self.__size ** 2
