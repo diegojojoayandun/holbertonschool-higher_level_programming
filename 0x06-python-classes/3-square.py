b72759bc69480f34b6826201621b7dea0676b997
diegojojoayandun <3922@holbertonschool.com>
2022-01-18 16:24:01 -0500

    Add task3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -9,9 +9,9 @@ class Square:
     """
     def __init__(self, size=0):
         """Initializes the data with validation"""
-        if not  type(size) == int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif (size < 0):
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
