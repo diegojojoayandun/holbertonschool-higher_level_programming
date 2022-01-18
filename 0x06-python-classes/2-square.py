54d04e5a1991cf63ae4bca497c31d16b578cd2f0
diegojojoayandun <3922@holbertonschool.com>
2022-01-18 16:17:25 -0500

    Fix docstrings with pylint

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,9 @@
 
 class Square:
     """defines a square
+
     Private instance attribute: size
+
     Instantiation with optional size:
         -def __init__(self, size=0):
     """
@@ -12,7 +14,8 @@ class Square:
 
     def __init__(self, size=0):
         """Initializes the data with validation"""
-        if not  type(size) == int:
+
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >=0")
