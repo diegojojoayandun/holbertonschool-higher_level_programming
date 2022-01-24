0bd59179a3acd9a0b77c776c3c0d276b1338eb86
diegojojoayandun <3922@holbertonschool.com>
2022-01-24 18:28:03 -0500

    Add Task2: 2-rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -45,7 +45,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height =
+        self.__height = value
 
     def area(self):
         """Calculates area of rectangle
