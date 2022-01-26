f5d4842b2a335d23e8532d066aa19ad840226731
diegojojoayandun <3922@holbertonschool.com>
2022-01-26 02:47:36 -0500

    del incorrect number

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""Module add-integer - adds 2 integers.
- a and b must be integers or floats, otherwise raise a TypeError
- exception with the message a must be an integer or b must be an integer
- a and b must be first casted to integers if they are float
+"""
+Module add-integer
+Adds two integer together
 """
 
 
@@ -10,8 +9,6 @@ def add_integer(a, b=98):
     """Returns the addition of a and b,
     or an error if a and b are not integers or floats
     """
-    if a == "":
-        raise TypeError("a must be an integer")
 
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
@@ -23,4 +20,5 @@ def add_integer(a, b=98):
     if type(b) is float:
         b = int(b)
 
-    return (a + b)
+    return a + b
+    
