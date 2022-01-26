fe42c38b98bd5907764c72aa42b6e506f6ee9ff1
diegojojoayandun <3922@holbertonschool.com>
2022-01-26 03:28:15 -0500

    fix code

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,9 +6,9 @@ def say_my_name(first_name, last_name=""):
     """
         print the first name and last name
     """
-    if type(first_name) is not (str):
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if type(last_name) is not (str):
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
     print("My name is {} {}".format(first_name, last_name))
