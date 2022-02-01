e2b21bb00201d976fd18673a902e13582ecafbcc
diegojojoayandun <3922@holbertonschool.com>
2022-02-01 17:17:00 -0500

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Module 9-add_item.
-Adds all arguments to a Python list,
-and then save them to a file.
-"""
+"""Module 7-add_item
+and then save them to a file."""
+
 
 import sys
 import json
 import os.path
+
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
