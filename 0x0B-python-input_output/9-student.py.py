cf69bafae66b9909c0fa8060601267b18fc73d69
diegojojoayandun <3922@holbertonschool.com>
2022-02-01 17:24:36 -0500

    FIX

--- 0x0B-python-input_output/9-student.py.py
+++ 0x0B-python-input_output/9-student.py.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-"""Module 9-student - student class
+"""
+Contains the clas "Student"
 """
 
 
 class Student:
-    """Class that defines a student by first_name
-    last_name and age.
-    """
-
+    """Representation of a student"""
     def __init__(self, first_name, last_name, age):
-        """Initializes the Student instance."""
-
+        """Initializes the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Retrieves a dictionary representation
-        of a Student instance"""
-
+        """returns a dictionary representation of a Student instance"""
         return self.__dict__
