a7c4bda95c8722caa3580e45e0c3e0ae0d5c0b45
diegojojoayandun <3922@holbertonschool.com>
2022-02-01 17:04:36 -0500

    fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,8 @@
 
 
 def read_file(filename=""):
-    """reads a text file (UTF8) and prints it to stdout"""
+    """reads a text file (UTF8) and prints
+    it to stdout"""
 
     with open(filename) as f:
         _read_file = f.read()
