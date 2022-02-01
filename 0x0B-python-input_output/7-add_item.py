7fcdc89c76cd03c9f856596a7dda79105834fa34
diegojojoayandun <3922@holbertonschool.com>
2022-02-01 17:12:50 -0500

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Module 7-add_item"""
 
+
 import sys
 import os.path
+
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
