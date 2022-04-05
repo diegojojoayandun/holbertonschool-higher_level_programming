ef6c7968e5c2394936290e52e083d13395848b92
diegojojoayandun <3922@holbertonschool.com>
2022-04-05 09:04:14 -0500

    change to arrow function

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 
 let argc = 0;
-
-exports.logMe = (element) => {
-  console.log(argc + ': ' + element);
-  argc++;
-};
+exports.logMe = (element) => console.log((argc++) + ': ' + element);
