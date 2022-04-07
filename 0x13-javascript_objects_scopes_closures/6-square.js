e9b30122618e53bdfccc4b4dcdbfe569b55061d6
diegojojoayandun <3922@holbertonschool.com>
2022-04-07 00:17:36 -0500

    fix print function

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,25 +5,17 @@ const square = require('./5-square');
 class Square extends square {
   charPrint (c) {
     if (c === undefined) {
-      for (let i = 0; i < this.height; i++) {
-        let j = 0;
-        for (; j < this.width; j++) {
-          process.stdout.write('X');
-        }
-        if (j === this.width) {
-          console.log('');
-        }
-      }
+      let i = 0;
+      do {
+        i++;
+        console.log('X'.repeat(this.width));
+      } while (i < this.height);
     } else {
-      for (let i = 0; i < this.height; i++) {
-        let j = 0;
-        for (; j < this.width; j++) {
-          process.stdout.write('C');
-        }
-        if (j === this.width) {
-          console.log('');
-        }
-      }
+      let i = 0;
+      do {
+        i++;
+        console.log('C'.repeat(this.width));
+      } while (i < this.height);
     }
   }
 }
