709f95d8df0ad01417858c4b80fa5404b3364c6f
diegojojoayandun <3922@holbertonschool.com>
2022-04-06 19:37:40 -0500

    handle error

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -7,4 +7,4 @@ const fileB = fs.readFileSync(process.argv[3], 'utf8');
 
 const fileC = fileA.concat(fileB);
 
-fs.writeFile(process.argv[4], fileC, 'utf8', (err, data) => { if (err) { console.log('no archivo', err); } });
+fs.writeFile(process.argv[4], fileC, 'utf8', (err, data) => { if (err) { console.log('err', err); } });
