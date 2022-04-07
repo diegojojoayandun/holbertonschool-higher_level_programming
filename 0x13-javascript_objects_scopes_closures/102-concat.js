349a4bcb3fcd4b12dab3289bca813ee062f1ad89
diegojojoayandun <3922@holbertonschool.com>
2022-04-06 19:34:41 -0500

    Add adnaced task 100, 102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,9 +2,9 @@
 
 const fs = require('fs');
 
-const contentA = fs.readFileSync(process.argv[2], 'utf8', (err, result) => (err) => console.log('error', err));
-const contentB = fs.readFileSync(process.argv[3], 'utf8', (err, result) => (err) => console.log('error', err));
+const fileA = fs.readFileSync(process.argv[2], 'utf8');
+const fileB = fs.readFileSync(process.argv[3], 'utf8');
 
-const contentC = contentA.concat(contentB);
+const fileC = fileA.concat(fileB);
 
-fs.writeFile(process.argv[4], contentC, 'utf8', (err, result) => (err) => console.log('error', err));
+fs.writeFile(process.argv[4], fileC, 'utf8', (err, data) => { if (err) { console.log('no archivo', err); } });
