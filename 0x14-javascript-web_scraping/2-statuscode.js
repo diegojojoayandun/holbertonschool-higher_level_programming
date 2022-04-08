ee1673f02a9046860d8e08d2fe473469c5171a4b
diegojojoayandun <3922@holbertonschool.com>
2022-04-08 14:14:55 -0500

    fix handle error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], (error, response, body) => console.log('Code:', response && response.statusCode));
+request(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log('error', error);
+  } else { console.log('Code:', response && response.statusCode); }
+});
