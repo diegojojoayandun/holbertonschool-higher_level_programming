a6717df0bb6793a42cf8ca98565a99c29780263c
diegojojoayandun <3922@holbertonschool.com>
2022-04-09 20:57:14 -0500

    semistandard compliant code

--- 0x14-javascript-web_scraping/6-complete_tasks.js
+++ 0x14-javascript-web_scraping/6-complete_tasks.js
@@ -8,14 +8,14 @@ request(url, (error, response, body) =>
   (error) ? console.error(error) : console.log(computeTasks(JSON.parse(body))));
 
 function computeTasks (jsonTasks) {
-  const completeTask = jsonTasks.filter((entries) => entries.completed === true);
+  const completedTask = jsonTasks.filter((entries) => entries.completed === true);
   const dict = {};
 
-  for (const i in completeTask) {
-    if (!dict[completeTask[i].userId]) {
-      dict[completeTask[i].userId] = 1;
+  for (const i in completedTask) {
+    if (!dict[completedTask[i].userId]) {
+      dict[completedTask[i].userId] = 1;
     } else {
-      dict[completeTask[i].userId]++;
+      dict[completedTask[i].userId]++;
     }
   }
   return dict;
