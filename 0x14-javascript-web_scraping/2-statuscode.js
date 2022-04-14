bc5d78c6b3adbc00ea0d9f05f4b7eb83eac286a6
diegojojoayandun <3922@holbertonschool.com>
2022-04-13 22:04:06 -0500

    Add first advanced task

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@ const request = require('request');
 request(process.argv[2], (error, response, body) => {
   if (error) {
     console.error('error', error);
-  } else { console.log('Code:', response && response.statusCode); }
+  } else { console.log('Code: ', response && response.statusCode); }
 });
