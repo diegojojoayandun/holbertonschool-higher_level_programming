247c6ec8343322b6ea55dca9d159eb1398b8d108
diegojojoayandun <3922@holbertonschool.com>
2022-04-13 22:01:13 -0500

    Add first advanced task

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) =>
+  (error) ? console.error(error) : console.log(computeTasks(JSON.parse(body))));
+
+function computeTasks (oTasks) {
+  const completedTask = oTasks.filter((entries) => entries.completed === true);
+  const dict = {};
+
+  for (const i in completedTask) {
+    if (!dict[completedTask[i].userId]) {
+      dict[completedTask[i].userId] = 1;
+    } else {
+      dict[completedTask[i].userId]++;
+    }
+  }
+  return dict;
+}
