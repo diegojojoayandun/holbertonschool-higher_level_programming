a96310e5a831c343ccf30af96a7c27c0d2fb03bc
diegojojoayandun <3922@holbertonschool.com>
2022-04-13 22:27:32 -0500

    ..

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+const argv = require('process').argv;
 
-try {
-  fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err, result) { if (err) console.log(err); });
-} catch (err) {
-  console.log(err);
-}
+fs.writeFile(argv[2], argv[3], 'utf8', (err) => {
+  if (err) console.log(err);
+});
