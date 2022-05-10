e49a0d05d7bf73baf3e29b2602aa9daee3bcb12d
diegojojoayandun <3922@holbertonschool.com>
2022-05-10 01:14:28 -0500

    Add _cursor.close()

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" lists all  states  from the database  hbtn_0e_0_usa"""
+"""
+lists all  states  from the database  hbtn_0e_0_usa
+"""
 
 
 if __name__ == '__main__':
@@ -7,16 +9,18 @@ if __name__ == '__main__':
     import MySQLdb
     import sys
 
-    db = MySQLdb.connect(host='localhost', port=3306,
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
                          user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
     _cursor = db.cursor()
-    _cursor.execute("SELECT * FROM states WHERE states.name LIKE 'N%' \
+    _cursor.execute("SELECT * FROM states WHERE states.name LIKE BINARY 'N%' \
         ORDER BY states.id ASC;")
     rows = _cursor.fetchall()
 
     for row in rows:
         print(row)
-
+    _cursor.close()
     db.close()
