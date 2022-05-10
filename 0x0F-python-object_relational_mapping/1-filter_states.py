96875f9581be6be23c4e097cee4f3f41fb75eabc
diegojojoayandun <3922@holbertonschool.com>
2022-05-10 01:08:32 -0500

    change shebang

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
     import sys
 
     db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+                         user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
 
     _cursor = db.cursor()
     _cursor.execute("SELECT * FROM states WHERE states.name LIKE 'N%' \
