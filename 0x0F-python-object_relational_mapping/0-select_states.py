0b4f8f244de0b291432addfc69a7d2db8fdd74f6
diegojojoayandun <3922@holbertonschool.com>
2022-05-10 00:52:43 -0500

    Fix imports

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ lists all  states  from the database  hbtn_0e_0_usa"""
 
-import MySQLdb
-import sys
-
 
 if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
 
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
