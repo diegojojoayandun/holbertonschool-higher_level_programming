6a25bffba98fa85915f2ee4ab6d75dbf42b3eec0
diegojojoayandun <3922@holbertonschool.com>
2022-05-10 01:01:29 -0500

    Remove _cursor.close(), line more than 80 lines and blank spaces after entry point

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ lists all  states  from the database  hbtn_0e_0_usa"""
 
+
 if __name__ == '__main__':
 
     import MySQLdb
@@ -10,8 +11,11 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     _cursor = db.cursor()
-    _cursor.execute("SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC;")
+    _cursor.execute("SELECT * FROM states WHERE states.name LIKE 'N%' \
+        ORDER BY states.id ASC;")
     rows = _cursor.fetchall()
 
     for row in rows:
         print(row)
+
+    db.close()
