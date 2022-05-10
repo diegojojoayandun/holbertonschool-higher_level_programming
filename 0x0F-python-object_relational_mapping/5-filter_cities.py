419c6fc49e951360003d45052453da5dd290478d
diegojojoayandun <3922@holbertonschool.com>
2022-05-10 01:36:56 -0500

    Fix output

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,20 +1,31 @@
-#!/usr/bin/env python3
-""" lists all  states  from the database  hbtn_0e_0_usa"""
+#!/usr/bin/python3
+"""
+lists all  states  from the database  hbtn_0e_0_usa
+"""
 
 if __name__ == '__main__':
 
     import MySQLdb
-    import sys
+    from sys import argv
 
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
 
     _cursor = db.cursor()
-    _cursor.execute("SELECT cities.id, cities.name, states.name\
+    _cursor.execute("SELECT cities.name\
                 FROM cities LEFT JOIN states\
                 ON states.id = cities.state_id\
-                WHERE states.name=%s ORDER BY cities.id ASC", (sys.argv[4], ))
+                WHERE states.name=%s ORDER BY cities.id ASC", (argv[4], ))
     rows = _cursor.fetchall()
 
+    _sep = ""
     for row in rows:
-        print(row)
+        print("{}{}".format(_sep, row[0]), end="")
+        _sep = ", "
+    print()
+
+    _cursor.close()
+    db.close()
