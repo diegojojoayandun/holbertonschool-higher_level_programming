c196d4ee6df78696418ab424e9ac469c5ea4876f
diegojojoayandun <3922@holbertonschool.com>
2022-05-18 14:51:05 -0500

    .

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -s "$1" -F POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD"
+curl -s --data "email=hr%40holbertonschool.com&subject=I will always+be+here+for+PLD" $1
