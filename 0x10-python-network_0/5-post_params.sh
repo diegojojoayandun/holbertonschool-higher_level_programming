ec2b334ad3abe4f0430ddefc2389732b3b929611
diegojojoayandun <3922@holbertonschool.com>
2022-05-18 16:12:56 -0500

    Fix -F

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request, and displays the body of the response
-curl -s  -X POST -F "email=hr@holbertonschool.com" -F "subject=I will always be here for PLD" "$1"
+curl -s "$1" -X POST -F "email=hr@holbertonschool.com" -F "subject=I will always be here for PLD"
