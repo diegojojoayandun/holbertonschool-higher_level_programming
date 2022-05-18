e81d417a4ff8fe950392cd9a193a7f412602a2bd
diegojojoayandun <3922@holbertonschool.com>
2022-05-18 13:47:57 -0500

    fix all

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -sX DELETE "$1"
+# takes in a URL and displays all HTTP methods the server will accept
+curl -sI "$1" | grep "Allow:" | sed -ne 's/^Allow: //p'
